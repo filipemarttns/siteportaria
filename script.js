fa1e43f68c33b65eeb446f823c4b2fe42c60cfde
filipemarttns <filipemarttns@gmail.com>
2025-04-26 07:29:17 -0300

    Save inicial

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const users = {
   "Trafego - Rouxinol": "trafego2025",
   "CCO - Rouxinol": "controlerotas2025",
   "Mecanica - Rouxinol": "mecanicarouxinol2025",
-  "Analista - Rouxinol": "analistarouxinol2026",
+  "2309": "eldin",
   "Limpeza - Rouxinol": "limopezarouxinol",
   "Manutenção - Rouxinol": "manutençãorouxinol2025",
   "1774": "Victor@123",
@@ -10,6 +10,7 @@ const users = {
   "Analista - Maxsim": "analistadados2",
   "1207": "j2025",
   "2396": "12082024",
+  "2587": "vitin",
 };
 
 function login() {
